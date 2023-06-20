b8f7a54396661e70c39c06afc7147c262db1245c
Alexandra <alexandrachrystal@gmail.com>
2023-06-21 00:56:05 +0400

    Updates

--- src/components/ProcessingPersonalData.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default class ProcessingPersonalData extends React.Component {
-    render() {
-        return (
-            <p>Обработка моих данных</p>
-        )
-    }
-}
